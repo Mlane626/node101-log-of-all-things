aa24c9a230a1b0cdd21a68b24de0950798977384
Mike L <mike.lane.626@gmail.com>
2023-02-02 11:38:00 -0800

    first commit

--- server/app.js
+++ server/app.js
@@ -1,20 +1,52 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const d = new Date();
+const csv = require('csvtojson')
+app.set('json spaces', 2);
+
+
+
 
 app.use((req, res, next) => {
-// write your logging code here
+    let agent =  req.header('User-Agent').replace(',' , '');
+    let time = d.toISOString();
+    let method = req.method;
+    let resource = req.path;
+    let version = ('HTTP/' + req.httpVersion);
+    let status = 200;
+    let data = agent + ',' + time + ',' +   method + ',' + resource + ',' + version + ',' + status + '\n';
+    console.log(data);
 
-});
+
+    // Agent,Time,Method,Resource,Version,Status
+//make all these into variables , 
+//find resouse 
+fs.appendFile('./server/log.csv' , data , 'utf8' ,
+function(err){
+    if (err) throw err;
+    next()
+})
+})
+
+
+
+// // write your logging code here
+
 
 app.get('/', (req, res) => {
+   
 // write your code to respond "ok" here
+res.send('ok');
+})
 
-});
-
+// // write your code to return a json object containing the log data here
 app.get('/logs', (req, res) => {
-// write your code to return a json object containing the log data here
-
+    csv()
+.fromFile('./server/log.csv')
+.then((csvObj)=>{
+res.send(csvObj)
+});
 });
 
 module.exports = app;
