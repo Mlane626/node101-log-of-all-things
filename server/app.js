15d1ace873a2e15b8a15357c1a8b0540b6a1831b
Mike L <mike.lane.626@gmail.com>
2023-02-02 11:44:32 -0800

    Cleans up formatting for syntax

--- server/app.js
+++ server/app.js
@@ -2,11 +2,9 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const d = new Date();
-const csv = require('csvtojson')
+const csv = require('csvtojson');
+
 app.set('json spaces', 2);
-
-
-
 
 app.use((req, res, next) => {
     let agent =  req.header('User-Agent').replace(',' , '');
@@ -18,35 +16,29 @@ app.use((req, res, next) => {
     let data = agent + ',' + time + ',' +   method + ',' + resource + ',' + version + ',' + status + '\n';
     console.log(data);
 
-
-    // Agent,Time,Method,Resource,Version,Status
-//make all these into variables , 
-//find resouse 
 fs.appendFile('./server/log.csv' , data , 'utf8' ,
-function(err){
-    if (err) throw err;
-    next()
-})
+    function(err){
+        if (err) throw err;
+        next();
+    })
 })
 
 
 
 // // write your logging code here
-
-
 app.get('/', (req, res) => {
-   
+
 // write your code to respond "ok" here
 res.send('ok');
 })
 
 // // write your code to return a json object containing the log data here
 app.get('/logs', (req, res) => {
-    csv()
-.fromFile('./server/log.csv')
-.then((csvObj)=>{
-res.send(csvObj)
-});
+        csv()
+    .fromFile('./server/log.csv')
+    .then((csvObj)=>{
+    res.send(csvObj)
+    });
 });
 
 module.exports = app;
